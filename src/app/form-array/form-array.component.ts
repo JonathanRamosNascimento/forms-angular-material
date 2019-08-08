4ee09afada6c16fd377158b0cb8eaa9c4edd4651
Jonathan Ramos <ramos.was.jonathan@gmail.com>
2019-08-07 23:45:52 -0300

    reactive forms: form array

---
+++ src/app/form-array/form-array.component.ts
@@ -0,0 +1,34 @@
+import { FormBuilder } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-form-array',
+  templateUrl: './form-array.component.html',
+  styleUrls: ['./form-array.component.css']
+})
+export class FormArrayComponent implements OnInit {
+
+  clientForm = this.fb.group({
+    firstName: [''],
+    lastName:[''],
+    address: this.fb.group({
+      street: [''],
+      city: [''],
+      state: ['']
+    }),
+    phone: [''],
+    children: this.fb.group({
+      name: [''],
+      age: ['']
+    })
+  });
+
+  constructor(private fb: FormBuilder) { }
+
+  ngOnInit() {
+  }
+
+  submit() {
+    console.log(this.clientForm.value);
+  }
+}
