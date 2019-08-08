4da9e566547947312d4367f3174ef0114f42a036
Jonathan Ramos <ramos.was.jonathan@gmail.com>
2019-08-08 02:47:04 -0300

    reactive forms, form array finish

--- src/app/form-array/form-array.component.ts
+++ src/app/form-array/form-array.component.ts
@@ -10,19 +10,17 @@ export class FormArrayComponent implements OnInit {
 
   clientForm = this.fb.group({
     firstName: [''],
-    lastName:[''],
+    lastName: [''],
     address: this.fb.group({
       street: [''],
       city: [''],
       state: ['']
     }),
     phones: this.fb.array(['']),
-    children: this.fb.group({
-      name: [''],
-      age: ['']
-    })
+    children: this.fb.array([])
   });
   phones = this.clientForm.get('phones') as FormArray;
+  children = this.clientForm.get('children') as FormArray;
 
   constructor(private fb: FormBuilder) { }
 
@@ -35,5 +33,14 @@ export class FormArrayComponent implements OnInit {
 
   addPhone() {
     this.phones.push(this.fb.control(''));
+  }
+
+  addChild() {
+    this.children.push(
+      this.fb.group({
+        name: this.fb.control(''),
+        age: this.fb.control('')
+      })
+    )
   }
 }
