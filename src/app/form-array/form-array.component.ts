80d33a9eb297fff52e5f9fb0561f2a2c3a734116
Jonathan Ramos <ramos.was.jonathan@gmail.com>
2019-08-08 02:19:18 -0300

    form array

--- src/app/form-array/form-array.component.ts
+++ src/app/form-array/form-array.component.ts
@@ -1,4 +1,4 @@
-import { FormBuilder } from '@angular/forms';
+import { FormBuilder, FormArray } from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -16,12 +16,13 @@ export class FormArrayComponent implements OnInit {
       city: [''],
       state: ['']
     }),
-    phone: [''],
+    phones: this.fb.array(['']),
     children: this.fb.group({
       name: [''],
       age: ['']
     })
   });
+  phones = this.clientForm.get('phones') as FormArray;
 
   constructor(private fb: FormBuilder) { }
 
@@ -30,5 +31,9 @@ export class FormArrayComponent implements OnInit {
 
   submit() {
     console.log(this.clientForm.value);
+  }
+
+  addPhone() {
+    this.phones.push(this.fb.control(''));
   }
 }
